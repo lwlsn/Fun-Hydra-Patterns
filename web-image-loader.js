cf483444b0c526fcf47838cb2f014bad2cb58b86
Lizzie <e-wilson1@hotmail.com>
2020-06-04 17:42:34 +0100

    Update web-image-loader.js
    t

--- web-image-loader.js
+++ web-image-loader.js
@@ -79,6 +79,42 @@ src(s0).color(1.5,1,1)
 
 
 
+// taake 3
+
+function loadImage() {
+  var myImage = document.createElement('img');
+  myImage.onload = ()=> s0.init({src:myImage, dynamic:false})
+  var num = Math.floor(Math.random()*Date.now())
+  myImage.crossOrigin = "Anonymous"
+  myImage.src = "https://scontent.flhr4-2.fna.fbcdn.net/v/t1.0-9/p960x960/101889556_10163572957625632_8942663058971426816_o.jpg?_nc_cat=110&_nc_sid=8024bb&_nc_ohc=69hQ3YSu_coAX_Wym4r&_nc_ht=scontent.flhr4-2.fna&_nc_tp=6&oh=0f6fb97fa21c96ba779c8f90eae5aef8&oe=5EFD6973"
+}
+
+loadImage()
+
+// mousedown plays audio and switches images
+window.document.body.addEventListener("mousedown", ()=>loadImage() )
+
+// timers
+setInterval( ()=> { loadImage() }, 7000)
 
 
+posY = ()=> (Math.sin(time/21)*.3)+.6
+
+src(s0).color(1.5,1,1)
+.modulateScrollY(osc(8,0).thresh( .5 ,0).rotate(Math.PI/2),.3,.016)
+.modulateScale(noise(10,0).thresh( .5 ,0).rotate(Math.PI/2), 2 )
+
+
+.add( osc(3,.08).thresh( .5 ,0).rotate(Math.PI/2) , .9 )
+.modulate(s0)
+.blend(s0)
+.scrollY(-0.32,-0.2)
+
+
+.mult( src(s0), .9 )
+.modulateHue( src(s0), 30)
+
+.out()
+
+//to -do: remove timers, mousedown func?>
 
