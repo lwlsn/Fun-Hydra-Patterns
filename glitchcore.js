0da8ab38582b28486d1692861021abe31f83bbca
Lizzie <e-wilson1@hotmail.com>
2022-02-28 18:11:11 +0000

    Create glitchcore.js

---
+++ glitchcore.js
@@ -0,0 +1,11 @@
+ 
+voronoi(50,0.15)
+  	.modulateScale(osc(8).rotate(Math.sin(time)),.5)
+  	.thresh(.8)
+	.modulateRotate(osc(70),.4)
+	.thresh(.7)
+  	.diff(src(o0).scale(1.8))
+	.modulateScale(osc(2).modulateRotate(o0,.74))
+	.diff(src(o0).rotate([-.012,.01,-.002,0]).scrollY(0,[-1/199800,0].fast(0.7)))
+	.brightness([-.02,-.17].smooth().fast(.5))
+	.out()
