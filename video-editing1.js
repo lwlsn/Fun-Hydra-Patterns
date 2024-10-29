7bc9d483daf4b64e9daf41ec43420b96aa6baa94
Lizzie <e-wilson1@hotmail.com>
2024-10-29 21:47:32 +0000

    Update video-editing1.js 📹

--- video-editing1.js
+++ video-editing1.js
@@ -2,18 +2,20 @@
 
 //s0.initScreen()
 
+
 a.setSmooth(.9)
 a.setCutoff(2)
 a.setScale(35)
 
 // (1)
 src(o0).layer(
-  src(s0).invert().luma().invert().color(0,0.9,0.2).modulateScale(o0)
+  src(s0).invert().luma().invert().color(0,()=>a.fft[0],0.2).modulateScale(o0)
 .modulateScale(gradient(),()=>Math.sin(time*2))
 .invert(()=>Math.sin(time/3))
 .layer(
   src(s0).invert().luma().invert().scale(.75))
 ).out()
+
 
 
 
