061281d57677d624e68fd9a0811c7ae09d67dd6f
Lizzie <e-wilson1@hotmail.com>
2024-10-29 21:02:43 +0000

    Create video-editing1.js

---
+++ video-editing1.js
@@ -0,0 +1,21 @@
+//  s0m3 c0d3
+
+//s0.initScreen()
+
+a.setSmooth(.9)
+a.setCutoff(2)
+a.setScale(35)
+
+// (1)
+src(o0).layer(
+  src(s0).invert().luma().invert().color(0,0.9,0.2).modulateScale(o0)
+.modulateScale(gradient(),()=>Math.sin(time*2))
+.invert(()=>Math.sin(time/3))
+.layer(
+  src(s0).invert().luma().invert().scale(.75))
+).out()
+
+
+
+
+
