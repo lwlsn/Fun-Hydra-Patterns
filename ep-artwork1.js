50717908c85b6b129fd27f75354290066fd30bd0
Lizzie <e-wilson1@hotmail.com>
2025-02-26 23:29:31 +0000

    Update ep-artwork1.js

--- ep-artwork1.js
+++ ep-artwork1.js
@@ -18,3 +18,25 @@ src(s0)
  .modulate(src(s0),0.1)
 	.blend(src(o0).scale(0.99))
 	.out()
+
+
+
+
+// Art2:
+
+
+await loadScript("https://hyper-hydra.glitch.me/hydra-text.js")
+s0.initImage("https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/481657277_10170700876655632_6671461364491854584_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XbUtgGxMHRUQ7kNvgHNWRv5&_nc_zt=23&_nc_ht=scontent-man2-1.xx&_nc_gid=AO5EPUC6-Bm5YJinw2VuwSs&oh=00_AYCB27s68nKw148l16jp8ipT19UQ2Dm013sSrDg3JNCRnw&oe=67C4363B")
+
+
+hydraText.font = "monaco"
+hydraText.lineWidth = "2%"
+
+str = "C’est pour le mieux, \n Nous ne finirons pas par nous faire du mal. \n Peut-être que c'est suffisant de rester comme ça, \n lumière dispersée. \n Je t’ai construit à partir du silence. \n quand allons-nous nous revoir, \n ou as-tu sacrifié quelque chose \n que quand nous aurons quatre-vingts ans, \n nous regretterons "
+
+src(s0)
+.layer(text(str, {fillStyle:"red"})).scale(0.78)
+ .modulate(src(s0),0.08)
+	.blend(src(o0).scale(0.99))
+	.out()
+
