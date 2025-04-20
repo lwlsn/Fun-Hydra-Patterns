0b67d55626bd69c55f284677902c16987f36aab4
Lizzie <e-wilson1@hotmail.com>
2025-04-20 21:38:18 +0100

    Update ep-artwork1.js

--- ep-artwork1.js
+++ ep-artwork1.js
@@ -40,3 +40,23 @@ src(s0)
 	.blend(src(o0).scale(0.99))
 	.out()
 
+
+// Art v3
+
+
+await loadScript("https://hyper-hydra.glitch.me/hydra-text.js")
+s0.initImage("https://scontent.ffab1-2.fna.fbcdn.net/v/t39.30808-6/489927611_1223595822655247_9067206627921477396_n.jpg?stp=dst-jpg_s1080x2048_tt6&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=5tsAGnjOuQAQ7kNvwHOMP9n&_nc_oc=AdmEKfILlrxjVTMRPlZlq90cp8NReZpn5jtbFkAV5KqjTx3YoJTxJebZ9LL24-kAM2WZpXkZx9TGaAecGE1sQEdA&_nc_zt=23&_nc_ht=scontent.ffab1-2.fna&_nc_gid=5s8pjfDpKD3ZF8ZtT2rucg&oh=00_AfEnu_2lOcQBB-_DLk8duj2gNO8q_Wbp5G5E6pH5L7eRFA&oe=680B3598")
+
+
+hydraText.font = "monaco"
+hydraText.lineWidth = "2%"
+
+
+src(s0)
+//.layer(text(str, {fillStyle:"red"})).scale(0.78)
+ .modulate(src(s0),0.01)
+	.blend(src(o0).scale(0.99))
+	.brightness(0.0298).contrast(1.1)
+	//.modulateScale(noise(2),-0.2)
+ 	.add(shape([4,5,6].fast(0.1).smooth(1),0.000001,[0.2,0.7,0.3].smooth(1)))
+	.out()
